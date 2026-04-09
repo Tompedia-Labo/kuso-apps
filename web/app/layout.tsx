79b569aa28697ce91442b1c024afed7e47e3c426
run01ia <208008422+run01ia@users.noreply.github.com>
2026-04-09 19:26:15 +0900

    トップページ作った

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+// const mplusrounded1c = M_PLUS_Rounded_1c({
+//   subsets: ["latin"],
+//   weight: ["100", "300", "400", "500", "700", "900"],
+// });
+//
 
 export const metadata: Metadata = {
   title: "Create Next App",
