e2c9d9190f8e587071175f5b33e19fcfc997fe8a
Morio Chiba <47474097+oriom-dev@users.noreply.github.com>
2026-04-11 12:19:53 +0900

    feat: refine

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,51 +3,68 @@ import style from "./style.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <main className={style.main}>
-        <h1 className={style.title}>クソWEBアプリ集</h1>
-        <div className={style.linkArea}>
-          <div className={style.linkBox}>
-            <a href="https://info.tompedia.jp">
-              <Image src="/logo.png" width={200} height={200} alt="Tompedia" />
-              Tompedia
-            </a>
-          </div>
-          <div className={style.linkBox}>
-            <a href="/the-day-after-tomorow">
-              <Image
-                src="/todo.png"
-                width={200}
-                height={200}
-                alt="The Day after Tomorow's ToDo"
-              />
-              ToDo
-            </a>
-          </div>
-          <div className={style.linkBox}>
-            <a href="/login">
-              <Image
-                src="/login.png"
-                width={200}
-                height={200}
-                alt="Login"
-              />
-              ログイン
-            </a>
-          </div>
-          <div className={style.linkBox}>
-            <a href="/tu-ton">
-              <Image
-                src="/logo.png"
-                width={200}
-                height={200}
-                alt="とんペディア入部フォーム"
-              />
-              とんペディア入部フォーム
-            </a>
-          </div>
+    <main className={style.main}>
+      <h1 className={style.title}>クソWEBアプリ集</h1>
+      <div className={style.linkArea}>
+        <div className={style.linkBox}>
+          <a href="/the-day-after-tomorow">
+            <Image
+              src="/todo.png"
+              width={200}
+              height={200}
+              alt="The Day after Tomorow's ToDo"
+            />
+            全てがクソなToDoリスト
+          </a>
         </div>
-      </main>
-    </div>
+        <div className={style.linkBox}>
+          <a href="/login">
+            <Image
+              src="/login.png"
+              width={200}
+              height={200}
+              alt="Login"
+            />
+            東北大生しかログインできないログインページ
+          </a>
+        </div>
+        <div className={style.linkBox}>
+          <a href="/tu-ton">
+            <Image
+              src="/logo.png"
+              width={200}
+              height={200}
+              alt="とんペディア入部フォーム"
+            />
+            モールス信号でしか入力できないとんペディア入部フォーム
+          </a>
+        </div>
+      </div>
+      <h1>とんペディアの紹介</h1>
+      <div className={style.linkArea}>
+        <div className={style.linkBox}>
+          <a href="https://info.tompedia.jp" target="_blank">
+            <Image
+              src="/logo.png"
+              width={200}
+              height={200}
+              alt="Tompedia"
+            />
+            とんペディアアプリについて
+          </a>
+        </div>
+        <div className={style.linkBox}>
+          <a href="https://labo.tompedia.jp" target="_blank">
+            <Image
+              src="/logo.png"
+              width={200}
+              height={200}
+              alt="Tompedia"
+            />
+            とんペディア_ラボについて
+          </a>
+        </div>
+      </div>
+    </main>
   );
 }
